ba83a7ff9922225a215211fe283aa8a145dd027b
Huy Nguyen <anhhuy.nguyen@nfq.com>
2025-04-08 16:58:50 +0700

    feat: :sparkles: add width input

---
+++ src/features/editor/components/editor/inputs/width.tsx
@@ -0,0 +1,19 @@
+import { Slider } from '@/components/ui/slider'
+
+interface Props {
+  value: number
+  onValueChange: (value: number) => void
+}
+
+export function WidthInput({ value, onValueChange }: Props) {
+  const sliderOnValueChange = (value: number[]) => onValueChange(value[0])
+  return (
+    <Slider
+      value={[value]}
+      onValueChange={sliderOnValueChange}
+      defaultValue={[50]}
+      max={100}
+      step={1}
+    />
+  )
+}
